fa86ce234fa702da28f62114b93e8b328072e044
broylesd16 <broylesd16@mycu.concord.edu>
2026-02-03 19:44:43 -0500

    Fixed the Slideshow
    Slideshow fixed and script.js moved to javascript folder.

---
+++ javascript/script.js
@@ -0,0 +1,52 @@
+let slideIndex = 0;
+let slideTimer;
+
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  autoSlides();
+} 
+
+function autoSlides() {
+  slideTimer = setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
+
+
+showSlides2(slideIndex);
+
+// Next/previous controls
+function nextSlide(n) {
+  clearTimeout(slideTimer);
+  showSlides2(slideIndex += n);
+  autoSlides();
+}
+
+function currentSlide(n) {
+  clearTimeout(slideTimer);
+  showSlides2(slideIndex = n);
+  autoSlides();
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
