a4fe69c3e7d5295c66ac97f94ae4162dae32bbd5
Emilio Bosma <csafety246@gmail.com>
2026-04-09 09:29:44 -0400

    Sources

--- javascript/getAnnouncements.js
+++ javascript/getAnnouncements.js
@@ -1,10 +1,17 @@
 // This file gets the announcements for each location page
 
+// Sources:
+// I actually wrote a fair amount of this myself using our previously written JS + PHP files as references.
+// I was having a hard time getting this to output so I ended up asking ChatGPT why I was recieving no output. Turns out
+// using of and in are two very different things in a for loop so I was only returning the index. 
+// I also had to look up "JS how to add new element to section" to figure out the append. I used the google AI example
+// to help so I could append each announcement as its own div inside of the announcements div.
+
 document.addEventListener("DOMContentLoaded", async function () {
     // Grab and initialize info from the HTML
     const titleElement = document.getElementById("title");
     let locationName = titleElement.textContent;
-    const announcementsSpace = document.getElementById("announcements");
+    const announcementsSpace = document.getElementById("announcementsy");
 
     locationName = locationName.replaceAll(' ', '_');
     locationName = locationName.toLowerCase();
